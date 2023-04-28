a68f9acdf94b3b1fc083751c9544aca7d7ec5266
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2023-04-28 12:51:18 -0500

    chore: remove unnecessary commits

--- Send/Program.cs
+++ Send/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using Microsoft.Extensions.Configuration;
+using RabbitMQ.Client;
+
+var config = new ConfigurationBuilder()
+.AddJsonFile($"appsettings.development.json")
+.Build();
+
+var factory = new ConnectionFactory
+{
+  HostName = "localhost",
+  Port = 5672,
+  UserName = config.GetSection("RabbitMQ").GetSection("Username").Value,
+  Password = config.GetSection("RabbitMQ").GetSection("Password").Value
+};
+
+using var conn = factory.CreateConnection();
+using var chan = conn.CreateModel();
+
+var queueName = "hello";
+
+chan.QueueDeclare(
+  queue: queueName,
+  durable: false,
+  exclusive: false,
+  autoDelete: false,
+  arguments: null
+);
+
+var message = "Hello World!";
+var body = Encoding.UTF8.GetBytes(message);
+
+chan.BasicPublish(
+  exchange: string.Empty,
+  routingKey: queueName,
+  basicProperties: null,
+  body: body
+);
+
+Console.WriteLine($" [x] Sent {message}");
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
